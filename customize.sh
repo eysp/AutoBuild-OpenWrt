9fa1fee907c5b9151fa126edd220bb24826e2e68
eysp <60641231+eysp@users.noreply.github.com>
2021-02-17 10:14:29 +0800

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -6,6 +6,8 @@
 # Youtube Channel: https://goo.gl/fvkdwm 
 #=================================================
 #1. Modify default IP
+sed -i 's/192.168.1.1/192.168.123.1/g' openwrt/package/base-files/files/bin/config_generate
+#2.luci setting
 cd openwrt
 git clone https://github.com/garypang13/openwrt-packages.git package/openwrt-packages
 rm -Rf package/openwrt-packages/default-settings
